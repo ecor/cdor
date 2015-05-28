7b3fa3db5c37e03ec72df2b9083f54bdd92a7176
ecor <emanuele.cordano@gmail.com>
2015-05-28 12:24:15 +0200

    Adding plotMaps_usingExtractMethod.R

--- inst/examples/plotMaps_usingExtractMethod.R
+++ inst/examples/plotMaps_usingExtractMethod.R
@@ -1,13 +1,17 @@
 
 ###############################################################################
 
-# plotMaps.R
+# plotMaps_usingExtractMethod.R
 #
 # This file is part of the prototype execercise of the ENERGY GROUP of
 # the latest EUROPEAS "First Climate Services Masterclass: Energy, Tourism, Agriculture in a changing climate" (http://www.euporias.eu) 
 # held in Bozen (South Tyrol, Italy), 18-22 May 2015.  
 # It manipulates netCDF climate data files with external tools, like  'cdo' and interfaces with "raster" 
 # package for data import/export and analysis.
+#
+#  NOTE: the use of "extract" method for RasterClass object lets Spatial Aggregation on Administrative Areas be executed in a very easy way!
+#  Keep warning on how to set correctly the arguments for "extract" method.
+#
 #
 #
 # author: Emanuele Cordano on 22-05-2015
@@ -39,7 +43,6 @@ library(raster)
 library(stringr)
 library(nccdosmart)
 
-###source('/Users/ecor/workspace_eclipse/MASTERCLASS/EMC15ENERGY/LOAD_ALL.R', chdir = TRUE)
 
 ## Remaps the netcdf in accordance with the period (PAST and FUTURE) set in the CONFIGURATION file
 outdir <- paste(Sys.getenv("HOME"),"temporary_nccdosmart",sep="/")
@@ -73,49 +76,28 @@ dsn <- system.file("misc/europe",package="nccdosmart")
 ##"/Users/ecor/workspace_eclipse/MASTERCLASS/EMC15ENERGY/data/europe"
 layer <-"europe_countries_ddg"
 
-europe <- readOGR(dsn=dsn,layer=layer)
-europe_raster <- rasterize(europe[,"NATION"],maps_aggr[[1]])
-nation_code <- sprintf("N%03d",as.vector(europe$NATION))
-maps_aggr_eu <- maps_aggr
-mask <- !is.na(europe_raster)
-for (it in names(maps_aggr_eu)) {
-	
-	mask <- !is.na(maps_aggr_eu[[it]]) & mask
-	
-}	
+###
+nameData <- "E_OBS_PAST"
+vectorMap <- readOGR(dsn=dsn,layer=layer)[,"NATION"]
 
-europe_raster_masked <- europe_raster
-europe_raster_masked[!mask] <- NA
-for (it in names(maps_aggr_eu)) {
-	
-	maps_aggr_eu[[it]][!mask] <- NA
-	
-}	
+rasterMap <- maps[[nameData]]
 
-dfr <- as.data.frame(stack(maps_aggr_eu),xy=TRUE)
-dfr_nation <- as.data.frame(europe_raster_masked,xy=FALSE)
-dfr_nation$NATION <- sprintf("N%04d",dfr_nation$NATION)
-europe$NATION_chr <- sprintf("N%04d",europe$NATION)
-#dfr <- cbind(df,df_nation)
-for (it in names(maps_aggr_eu)) {
-	
-	europe@data[,it] <- NA
-	out <- tapply(X=dfr[,it],INDEX=dfr_nation$NATION,FUN=mean)
-	missing_names <- europe$NATION_chr[!(europe$NATION_chr %in% names(out))]
-	out[missing_names] <- NA
-	print(out)
-	europe@data[,it] <- out[europe$NATION_chr]
-	
-}
-for (it in names(maps_aggr_eu)) {
-	
-	pdffile <- paste(DirPlot,"/",it,"_eu.pdf",sep="")
-	pdf(pdffile)
-	p <- spplot(europe,it,main=it,col.regions=rev(color))
-	print(p)
-	
-	dev.off()
-	
-	
-}
+print(rasterMap)
+print(vectorMap)
+##
+## 
+## help(extract) to see documentation on 'extract' method
+## or see
+## http://www.inside-r.org/packages/cran/raster/docs/extract
 
+AggregatedData <- extract(x=rasterMap,y=vectorMap,fun=mean,sp=TRUE,na.rm=TRUE)
+
+it <- "X1997.05.01"
+
+p <- spplot(AggregatedData,it,main=paste(nameData,it,sep=" "),col.regions=rev(color))
+print(p)
+
+
+
+
+#####

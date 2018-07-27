3b9e8e5dc9720a2234eddad22279da1e6e6c78a0
Emanuele Cordano <cordaem@d01ri1500655.ies.jrc.it>
2018-07-27 16:07:09 +0200

    updates

--- R/cdo.sellonlatbox.R
+++ R/cdo.sellonlatbox.R
@@ -25,8 +25,8 @@ NULL
 #' \dontrun{
 #' ### 
 #'  url <- "ftp://ftp.chg.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/global_daily/netcdf/p05/chirps-v2.0.2005.days_p05.nc"
-#'  temp <-- resterTmFile()
-#'  x <- '/H01/SHAREDWORK/ACEWATER2/data/db/climate/chirps/data/daily/netcdf_global/p05/chirps-v2.0.2005.days_p05.nc' 
+#'  temp <-- rasterTmFile()
+#' 
 #'  y <- extent(c(-17.6608, -5.727799, 10.17488, 18.62197))
 #' 
 #' 
